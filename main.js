5792bce0fa8353a28be1d11795db50fdd83e8c64
Robert Leedy <leedyrj@gmail.com>
2018-06-05 09:08:20 -0500

    Finsihed lightning exercise 2

---
+++ main.js
@@ -0,0 +1,10 @@
+// 1. Write a function that takes two arguments. This function should return the concatenated value of the two arguments.
+const twoArgs = (argOne, argTwo) => {
+    return (`${argOne} ${argTwo}`)
+}
+
+// 2. Call the function with your first name and last name as the two arguments.
+const fullName = twoArgs('Robert', 'Leedy')
+// 3. Console log the return value.
+
+console.log(fullName)
